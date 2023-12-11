904df5adf2322c7323e9d771458bd65bf259d0e2
Егор Михайлович <81710799+grawwe@users.noreply.github.com>
2023-12-11 19:43:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let offset = 0
 
 leftBtn.addEventListener('click', function(){
     offset -= 121.5
-    if (offset < 0) offset = 520
+    if (offset < 0) offset = 487
     sliderLine.style.right = offset + 'px'
 })
 
 rightBtn.addEventListener('click', function(){
     offset += 121.5
-    if (offset > 520) offset = 0
+    if (offset > 500) offset = 0
     sliderLine.style.right = offset + 'px'
 })
